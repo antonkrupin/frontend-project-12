aced156955a078ea4f110c0367e9ea4b6ead863e
Anton Krupin <krupin_anton@mail.ru>
2022-09-12 19:50:40 +0300

    uninstall formik

--- frontend/src/routes/login.jsx
+++ frontend/src/routes/login.jsx
@@ -1,7 +1,37 @@
-export default function Login() {
+//import React from 'react';
+import { useFormik } from 'formik';
+
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      userName: '',
+      password: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
-    <div className="container-fluid h-100 mt-5">
-      <h2 className="text-primary">Login</h2>
-    </div>
+    <form onSubmit={formik.onSubmit}>
+      <label htmlFor="userName">Имя пользователя</label>
+      <input
+        id="userName"
+        name="userName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.userName} 
+      />
+      <label htmlFor="password">Пароль</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+      <button type="submit">Войти</button>
+    </form>
   );
 }
+
+export default Login;
