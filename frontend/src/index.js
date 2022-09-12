d729f0760e2001695d2e90df231bf8d73b978dde
Anton Krupin <krupin_anton@mail.ru>
2022-09-12 19:51:26 +0300

    replace import './index.css'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import Login from './routes/login';
 import Page404 from './routes/page404';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
