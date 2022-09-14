c4f64e8ac172cc44de35ff71c14f48410a5265af
antonkrupin <krupin_anton@mail.ru>
2022-09-14 12:23:20 +0300

    create errors locale

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+	translation: {
+		errors: {
+			userName: {
+				length: 'Имя пользователя меньше 5 знаков',
+				required: 'Поле обязательно для заполнения',
+			},
+			password : {
+				length: 'Пароль должен быть не меньше 8 знаков',
+				required: 'Поле обязательно для заполнения',
+				number: 'Пароль должен содержать цифру',
+				lowerCaseLetter: 'Пароль должен содеражть букву',
+				upperCaseLetter: 'Пароль должен содержать заглавную букву',
+			}
+		},
+	},
+};
