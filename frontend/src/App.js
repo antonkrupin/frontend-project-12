9d1322d8b5215619f0665cebfb08428b2c7ca88d
Anton Krupin <krupin_anton@mail.ru>
2022-09-14 20:39:32 +0300

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,46 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Outlet, Link } from 'react-router-dom';
+import Login from './routes/login';
+import Page404 from './routes/page404';
+import Main from './routes/main';
 import './App.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Chat</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/login">Login</Link> |{" "}
-      </nav>
-      <Outlet />
-    </div>
-  );
+const TestContext = React.createContext({});
+export default class App extends React.Component {
+  state = {
+    userName: '',
+    password: '',
+    token: '',
+  }
+
+  render () {
+    return (
+      <div>
+        <TestContext.Provider value = {this.state}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="login" element={<Login />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </BrowserRouter>
+        </TestContext.Provider>
+      </div>
+    );
+  }
 }
+
+/*
+<Route path="login" render={() => (
+                this.state.isLogged ? (<Navigate replace to="/"/>) : (<Login/>))} />
+<Route path="/" element={<Navigate replace to="/home" />} />
+ <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  </BrowserRouter>
+
+*/
