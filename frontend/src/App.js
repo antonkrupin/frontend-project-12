21fb0d561921b95009dc62b555ea8eb0f95f6bca
antonkrupin <krupin_anton@mail.ru>
2022-09-19 11:21:47 +0300

    Add AuthProvider Add PrivateRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,58 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate, Redirect  } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate, useLocation  } from 'react-router-dom';
 import { Outlet, Link } from 'react-router-dom';
 import Login from './routes/login';
 import Page404 from './routes/page404';
 import Main from './routes/main';
 import './App.css';
+import AuthContext from './contexts/index.jsx';
+import useAuth from './hooks/index.jsx';
 
-const TestContext = React.createContext({});
 
-export default class App extends React.Component {
-  state = {
-    userName: '',
-    password: '',
-    token: '',
-    test: false,
-  }
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  render () {
-    return (
-      <div>
-        <TestContext.Provider value = {this.state}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={this.state.test ? <Main/> : <Navigate replace to="login" />}/>
-              <Route path="login" element={<Login />} />
-              <Route path="*" element={<Page404 />} />
-            </Routes>
-          </BrowserRouter>
-        </TestContext.Provider>
-      </div>
-    );
-  }
-}
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
 
-/*
-<Route path="/" element={<Navigate replace to="/home" />} 
-<Navigate replace to="/home" />
-{loggedIn ? <Redirect to="/dashboard" /> : <PublicHomePage />}
-<Route path="login" render={() => (
-                this.state.isLogged ? (<Navigate replace to="/"/>) : (<Login/>))} />
-<Route path="/" element={<Navigate replace to="/home" />} />
- <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="login" element={<Login />} />
-      <Route path="*" element={<Page404 />} />
-    </Routes>
-  </BrowserRouter>
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-*/
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+
+  return (
+    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+  );
+};
+
+const App = () => (
+	<AuthProvider>
+		<div>
+			<BrowserRouter>
+				<Routes>
+				<Route
+            path="/"
+            element={(
+              <PrivateRoute>
+                <Main />
+              </PrivateRoute>
+            )}
+          />
+					<Route path="login" element={<Login />} />
+					<Route path="*" element={<Page404 />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	</AuthProvider>
+);
+
+export default App;
