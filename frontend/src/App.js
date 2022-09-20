5f241215f47fe172d3b3672cb03005a6041c527b
antonkrupin <krupin_anton@mail.ru>
2022-09-20 10:08:26 +0300

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useLocation  } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Button, Navbar, Nav } from 'react-bootstrap';
 import { Outlet, Link } from 'react-router-dom';
 import Login from './routes/login';
 import Page404 from './routes/page404';
 import Main from './routes/main';
+import Test from './routes/test'
 import './App.css';
 import AuthContext from './contexts/index.jsx';
 import useAuth from './hooks/index.jsx';
@@ -49,6 +50,14 @@ const AuthButton = () => {
 const App = () => (
 	<AuthProvider>
 			<BrowserRouter>
+				<Navbar bg="light" expand="lg">
+					<Navbar.Brand as={Link} to="/">Main page</Navbar.Brand>
+					<Nav className="mr-auto">
+						<Nav.Link as={Link} to="/login">Login page</Nav.Link>
+						<Nav.Link as={Link} to="/test">Test page</Nav.Link>
+					</Nav>
+					<AuthButton />
+				</Navbar>
 				<Routes>
 				<Route
             path="/"
@@ -60,6 +69,7 @@ const App = () => (
           />
 					<Route path="login" element={<Login />} />
 					<Route path="*" element={<Page404 />} />
+					<Route path="test" element ={<Test />} />
 				</Routes>
 			</BrowserRouter>
 	</AuthProvider>
