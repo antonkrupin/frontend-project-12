3e1449ac46720973a72fa3e185db75fd64148d7b
antonkrupin <krupin_anton@mail.ru>
2022-09-20 12:15:27 +0300

    create form with react-bootstrap

--- frontend/src/routes/login.jsx
+++ frontend/src/routes/login.jsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useRef, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import i18next from 'i18next';
 import { useFormik } from 'formik';
 import axios from 'axios';
 import * as yup from 'yup';
 import useAuth from '../hooks';
-import { Alert } from 'react-bootstrap';
+import { Form, Alert, Button, Overlay } from 'react-bootstrap';
 
 import resources from '../locales/index';
 import '../styles/login.css'
@@ -40,19 +40,29 @@ const Login = () => {
 
   const formik = useFormik({
     initialValues: {
-      userName: '',
+      userName: 'text',
       password: '',
     },
     validationSchema: validationSchema,
     onSubmit: async (values) => {
 			const response = await axios.post('/api/v1/login', { username: values.userName, password: values.password }).catch((error) => {
-				setAlert('the username or password is incorrect')
+				setAlert('Неверные имя пользователя или пароль');
+				setShow(!show);
 			});
 			localStorage.setItem('userId', JSON.stringify(response.data));
 			logIn();
 			navigate('/');
     },
   });
+
+	const [show, setShow] = useState(false);
+  const target = useRef(null);
+
+	const inputRef = useRef();
+
+	useEffect(() => {
+		inputRef.current.focus();
+	}, [])
 
   const setInputValue = useCallback(
     (key, value) =>
@@ -63,17 +73,48 @@ const Login = () => {
     [formik]
   );
 
-	let alertBlock;
+	return (
+		<div className="main">
+			<Form onSubmit={formik.handleSubmit} >
+				<Overlay target={target.current} show={show} placement="bottom">
+					{({ placement, arrowProps, show: _show, popper, ...props }) => (
+						<div
+							{...props}
+							style={{
+								position: 'absolute',
+								left: '-100px',
+								backgroundColor: 'rgba(255, 100, 100, 0.95)',
+								padding: '2px 10px',
+								color: 'white',
+								borderRadius: 3,
+								...props.style,
+							}}
+						>
+							{alert}
+						</div>
+					)}
+				</Overlay>
+				<Form.Group className="mb-3" controlId="userName">
+					<Form.Control 
+						type="text" 
+						placeholder={"Введите имя пользователя"} 
+						name="userName" 
+						onChange={(e) => setInputValue("userName", e.target.value)} 
+						required
+						ref={inputRef}
+					/>
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="password" >
+					<Form.Control type="text" placeholder="Введите пароль" name="password" onChange={(e) => setInputValue("password", e.target.value)} required />
+				</Form.Group>
+				<Button className="w-100" variant="primary" type="submit">
+					Войти
+				</Button>
+			</Form>
+		</div>
+	)
 
-  if (alert !== '') {
-    alertBlock = (
-      <Alert variant="danger">
-        {alert}
-      </Alert>
-    );
-  }
-
-  return (
+  /*return (
     <div className="main">
       <div className="wrapper">
         <div className="test">
@@ -115,7 +156,7 @@ const Login = () => {
 				</div>
       </div>
     </div>
-  );
+  );*/
 }
 
 export default Login;
