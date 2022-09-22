1054629b934b565fdeb3512914b8fad72e8a6af9
antonkrupin <krupin_anton@mail.ru>
2022-09-22 15:43:57 +0300

    change makrup

--- frontend/src/components/channelsList.jsx
+++ frontend/src/components/channelsList.jsx
@@ -4,17 +4,36 @@ import { Nav } from 'react-bootstrap';
 import { getChannels, setActiveChannel } from '../slices/channelsReducer';
 
 const ChannelsList = () => {
-	const channels = useSelector((state) => state.channels.value);
+	const channels = useSelector((state) => state.channels.channels);
+
 	const dispatch = useDispatch();
 
 	const getChannelName = (e) => {
-		dispatch(setActiveChannel(e.target.textContent));
+		const channelName = e.target.textContent.slice(2);
+		const activeChannel = channels.filter((channel) => channel.name === channelName ?? channel);
+		dispatch(setActiveChannel(activeChannel[0]));
 	}	
 	
 	return (
 		<>
-		<h4>Каналы</h4>
-		{ channels.map((item) => <Nav.Link key={item.id} href="#" onClick={getChannelName}># {item.name}</Nav.Link> ) }
+		<div className="d-flex justify-content-between mb-2 ps-4 pe-2">
+			<span>Каналы</span>
+			<button type="button" className="p-0 text-primary btn btn-group-vertical">
+				<svg 
+					xmlns="http://www.w3.org/2000/svg" 
+					viewBox="0 0 16 16" 
+					width="20" 
+					height="20" 
+					fill="currentColor">
+						<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
+						<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+				</svg>
+				<span className="visually-hidden">+</span>
+			</button>
+		</div>
+			<ul className="nav flex-column nav-pills nav-fill px-2">
+				{ channels.map((item) => <li key={item.id} className="nav-item w-100" onClick={getChannelName}><button type="button" className="w-100 rounded-0 text-start btn"><span className="me-1">#</span> {item.name}</button></li> ) }
+			</ul>
 		</>
 	)
 }
