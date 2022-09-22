2ce78f1f9ba27f3995340392b2a44f50d02a753f
antonkrupin <krupin_anton@mail.ru>
2022-09-22 15:42:21 +0300

    add messages: messagesReducer

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from '../slices/channelsReducer.js';
+import messagesReducer from './messagesReducer.js';
 
 export default configureStore({
 	reducer: {
 		channels: channelsReducer,
+		messages: messagesReducer,
 	},
 });
