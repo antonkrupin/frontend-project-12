dbfd38c5ce007c5874949b6adae067e4fab69805
antonkrupin <krupin_anton@mail.ru>
2022-09-23 13:02:13 +0300

    remove 	import { Nav } from 'react-bootstrap';

--- frontend/src/components/channelsList.jsx
+++ frontend/src/components/channelsList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Nav } from 'react-bootstrap';
 import { getChannels, setActiveChannel } from '../slices/channelsReducer';
 
 const ChannelsList = () => {
