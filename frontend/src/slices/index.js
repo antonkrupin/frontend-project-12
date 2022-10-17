0e7e9670310258ffa004e69a2ee4a25f4892a218
antonkrupin <krupin_anton@mail.ru>
2022-10-17 16:06:19 +0300

    add modalsReducer

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from '../slices/channelsReducer.js';
 import messagesReducer from './messagesReducer.js';
+import modalsReducer from './modalsReducer.js';
+
 
 export default configureStore({
 	reducer: {
 		channels: channelsReducer,
 		messages: messagesReducer,
+		modals: modalsReducer,
 	},
 });
