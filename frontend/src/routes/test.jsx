408beaf5e5ffa9dce203b40c042d6ba97d621116
Anton Krupin <krupin_anton@mail.ru>
2022-10-17 21:10:23 +0300

    change

--- frontend/src/routes/test.jsx
+++ frontend/src/routes/test.jsx
@@ -1,8 +1,13 @@
 import axios from 'axios';
+import TestDropDown from '../components/testDropDown';
 
 const Test = () => {
-	axios.post('/api/v1/signup', { username: 'newuser', password: '123456' });
-	return <h1>Test page</h1>
+	//axios.post('/api/v1/signup', { username: 'newuser', password: '123456' });
+	return (
+		<>
+			<TestDropDown />
+		</>	
+	)
 }
 
 export default Test;
