06fa9d4ed6df9f38254f5b58b300993e3b7b2182
Anton Krupin <krupin_anton@mail.ru>
2022-10-17 21:09:42 +0300

    change AddChannelModal

--- frontend/src/components/modals/AddChannelModal.jsx
+++ frontend/src/components/modals/AddChannelModal.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import _ from 'lodash';
 import { Form, InputGroup, Modal, Button } from 'react-bootstrap';
 
 import { addChannelModalShow } from '../../slices/modalsReducer';
@@ -9,36 +10,45 @@ const AddChannelModal = (props) => {
 
 	const { socket } = props;
 
+	const channelsNames = useSelector((state) => state.channels.channels).map(({name}) => name);
+
 	const isAddChannelModalShow = useSelector((state) => state.modals.isAddChannelModalShow);
 
 	const [name, setChannelName] = useState(null);
 
 	const addChannelHanlder = (e) => {
 		e.preventDefault();
-		socket.emit('newChannel', { name });
-		dispatch(addChannelModalShow())
+		if (!_.includes(channelsNames, name)) {
+			socket.emit('newChannel', { name });
+			dispatch(addChannelModalShow())
+		} else {
+			console.log('double channel name');
+		}
+		
 	}
 
 	return (
 		<>
-			<Modal show={isAddChannelModalShow} onHide={() => dispatch(addChannelModalShow())}>
-        <Modal.Header closeButton>
-          <Modal.Title>Добавить канал</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-					<InputGroup className="mb-3" onChange={(e) => setChannelName(e.target.value)}>
+		<Modal show={isAddChannelModalShow} onHide={() => dispatch(addChannelModalShow())}>
+			<Modal.Header closeButton >
+				<Modal.Title>Добавить канал</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<Form className="is-invalid" >
+					<InputGroup className="mb-3 is-invalid required" onChange={(e) => setChannelName(e.target.value)}>
 						<Form.Control aria-label="chartName" />
 					</InputGroup>
-				</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => dispatch(addChannelModalShow())}>
-            Отменить
-          </Button>
-          <Button variant="primary" onClick={addChannelHanlder}>
-            Отправить
-          </Button>
-        </Modal.Footer>
-      </Modal>
+				</Form>
+			</Modal.Body>
+			<Modal.Footer className="border-top-0">
+				<Button variant="secondary" onClick={() => dispatch(addChannelModalShow())}>
+					Отменить
+				</Button>
+				<Button variant="primary" onClick={addChannelHanlder}>
+					Отправить
+				</Button>
+			</Modal.Footer>
+		</Modal>
 		</>
 	)
 };
