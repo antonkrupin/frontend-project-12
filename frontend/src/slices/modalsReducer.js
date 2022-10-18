f607236dff8e17257591dc9e0f006ee69fccc7f7
antonkrupin <krupin_anton@mail.ru>
2022-10-18 15:18:01 +0300

    add rennameChannelModalShow

--- frontend/src/slices/modalsReducer.js
+++ frontend/src/slices/modalsReducer.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
 	isAddChannelModalShow: false,
 	isDeleteChannelModalShow: false,
+	isRenameChannelModalShow: false,
 };
 
 const modalsSlice = createSlice({
@@ -13,14 +14,18 @@ const modalsSlice = createSlice({
 			state.isAddChannelModalShow = !state.isAddChannelModalShow;
 		},
 		deleteChannelModalShow: (state, action) => {
-			state.isDeleteChannelModalShow = !state.isAddChannelModalShow;
-		}
+			state.isDeleteChannelModalShow = !state.isDeleteChannelModalShow;
+		},
+		renameChannelModalShow: (state, action) => {
+			state.isRenameChannelModalShow = !state.isRenameChannelModalShow;
+		},
 	}
 });
 
 export const {
 	addChannelModalShow,
 	deleteChannelModalShow,
+	renameChannelModalShow,
 } = modalsSlice.actions;
 
 export default modalsSlice.reducer;
