81c315b0cdb551d53299a681478ede090759956d
antonkrupin <krupin_anton@mail.ru>
2022-10-19 15:35:00 +0300

    add errors

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -2,9 +2,10 @@
 export default {
 	translation: {
 		errors: {
-			userName: {
+			username: {
 				length: 'Имя пользователя меньше 5 знаков',
 				required: 'Поле обязательно для заполнения',
+				maxLength: 'Имя пользователя не больше 20 знаков',
 			},
 			password : {
 				length: 'Пароль должен быть не меньше 5 знаков',
@@ -12,6 +13,11 @@ export default {
 				number: 'Пароль должен содержать цифру',
 				lowerCaseLetter: 'Пароль должен содеражть букву',
 				upperCaseLetter: 'Пароль должен содержать заглавную букву',
+			},
+			authorization: {
+				wrong: 'Неверные имя пользователя или пароль',
+				confirmPassword: 'Пароли должны совпадать',
+				userExist: 'Такой пользователь уже существует',
 			}
 		},
 	},
