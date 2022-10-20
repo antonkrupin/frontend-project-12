7b8235eaf3f606a89bea05e2ecfd9a8d478cfa79
antonkrupin <krupin_anton@mail.ru>
2022-10-20 16:13:49 +0300

    change

--- frontend/src/components/modals/RenameChannelModal.jsx
+++ frontend/src/components/modals/RenameChannelModal.jsx
@@ -6,6 +6,7 @@ import { Form, InputGroup, Modal, Button } from 'react-bootstrap';
 import i18 from '../../asserts/i18';
 
 import { renameChannelModalShow } from '../../slices/modalsReducer';
+import { renameChannelId } from '../../slices/channelsReducer';
 
 const RenameChannelModal = (props) => {
 	const dispatch = useDispatch();
@@ -20,22 +21,30 @@ const RenameChannelModal = (props) => {
 
 	const isRenameChannelModalShow = useSelector((state) => state.modals.isRenameChannelModalShow);
 	
-	const [name, setChannelName] = useState('');
+	const [name, setChannelName] = useState();
 
 	const [error, setError] = useState('');
-	
+
 	const renameChannelHandler = (e) => {
 		e.preventDefault();
 		if (!_.includes(channelsNames, name)) {
 			socket.emit('renameChannel', { id: channelId, name });
-			dispatch(renameChannelModalShow())
+			setChannelName('');
+			setError('');
+			dispatch(renameChannelModalShow());
 		}
 		setError(i18.t('errors.channels.renameChannel'));
 	}
 
+	const cancelRenameChannelHandler = () => {
+		setChannelName('');
+		setError('');
+		dispatch(renameChannelModalShow());
+	}
+
 	return (
 		<>
-		<Modal show={isRenameChannelModalShow} onHide={() => dispatch(renameChannelModalShow())}>
+		<Modal show={isRenameChannelModalShow} onHide={cancelRenameChannelHandler}>
 			<Modal.Header closeButton >
 				<Modal.Title>Переименовать канал</Modal.Title>
 			</Modal.Header>
@@ -52,7 +61,7 @@ const RenameChannelModal = (props) => {
 				</form>
 			</Modal.Body>
 			<Modal.Footer className="border-top-0">
-				<Button variant="secondary" onClick={() => dispatch(renameChannelModalShow())}>
+				<Button variant="secondary" onClick={cancelRenameChannelHandler}>
 					Отменить
 				</Button>
 				<Button variant="primary" onClick={renameChannelHandler}>
