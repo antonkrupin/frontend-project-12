1584e853f1d66d7ab90c04c697a0ccdc510d078b
antonkrupin <krupin_anton@mail.ru>
2022-10-25 14:23:12 +0300

    change

--- frontend/src/components/dropDownMenu.jsx
+++ frontend/src/components/dropDownMenu.jsx
@@ -21,9 +21,9 @@ const DropDownMenu = (props) => {
 		dispatch(deleteChannelModalShow());
 	}
 
-  return (
-    <Dropdown as={ButtonGroup} className="d-flex">
-      <Button onClick={props.onClick} id={props.dropDownId} className={props.channelNameClass}># {props.channelName}</Button>
+  {/*return (
+    <Dropdown as={ButtonGroup} className="d-flex border-0">
+      <button onClick={props.onClick} id={props.dropDownId} className={props.channelNameClass}># {props.channelName}</button>
 
       <Dropdown.Toggle split className={props.dropDownClass} id={props.dropDownId} />
 
@@ -32,7 +32,63 @@ const DropDownMenu = (props) => {
         <Dropdown.Item onClick={renameChannelHandler} href="#">{i18n.t('ui.dropDownMenu.rename')}</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
-  )
+	)*/}
+	{/*return (
+		<div className="d-flex border-0 btn-group">
+			<button onClick={props.onClick} id={props.dropDownId} className={props.channelNameClass}># {props.channelName}</button>
+			<button type="button" className={props.dropDownClass} id={props.dropDownId} data-bs-toggle="dropdown" aria-expanded="false">
+				<span className="visually-hidden">Toggle Dropdown</span>
+			</button>
+			<ul className="dropdown-menu">
+				<li><button onClick={deleteChannelHandler} className="dropdown-item" href="#">{i18n.t('ui.dropDownMenu.delete')}</button></li>
+				<li><button onClick={renameChannelHandler} className="dropdown-item" href="#">{i18n.t('ui.dropDownMenu.rename')}</button></li>
+			</ul>
+		</div>
+	)*/}
+	return (
+		<div className="d-flex border-0 btn-group">
+			<button 
+				onClick={props.onClick}
+				id={props.dropDownId}
+				className={props.channelNameClass}>
+				# {props.channelName}
+			</button>
+			<button 
+				type="button"
+				className={props.dropDownClass}
+				id={props.dropDownId}
+				data-bs-toggle="dropdown"
+				aria-expanded="false">
+				<span className="visually-hidden">Toggle Dropdown</span>
+			</button>
+			<ul className="dropdown-menu">
+				<li>
+					<button
+						onClick={deleteChannelHandler}
+						className="dropdown-item"
+						href="#">
+							{i18n.t('ui.dropDownMenu.delete')}
+					</button>
+				</li>
+				<li>
+					<button
+						onClick={renameChannelHandler}
+						className="dropdown-item"
+						href="#">
+							{i18n.t('ui.dropDownMenu.rename')}
+					</button>
+				</li>
+			</ul>
+		</div>
+	)
 };
 
 export default DropDownMenu;
+
+/*
+
+<button type="button" className="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+    <span className="visually-hidden">Toggle Dropdown</span>
+  </button>
+
+*/
