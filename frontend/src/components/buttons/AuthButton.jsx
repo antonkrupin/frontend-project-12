39b8d8b3e0995f69b2d064691123b7e328291320
Anton Krupin <krupin_anton@mail.ru>
2022-11-02 21:06:57 +0300

    delete comments

--- frontend/src/components/buttons/AuthButton.jsx
+++ frontend/src/components/buttons/AuthButton.jsx
@@ -1,4 +1,3 @@
-import { useLocation, Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
 import i18n from '../../asserts/i18';
@@ -6,7 +5,6 @@ import useAuth from '../../hooks/index'
 
 const AuthButton = () => {
   const auth = useAuth();
-  //const location = useLocation();
 
   return (
     auth.loggedIn && <Button onClick={auth.logOut}>{i18n.t('ui.buttons.logout')}</Button>
@@ -14,8 +12,3 @@ const AuthButton = () => {
 };
 
 export default AuthButton;
-/*
-
-: <Button as={Link} to="/login" state={{ from: location }}>{i18n.t('ui.buttons.login')}</Button>
-
-*/
