837435304a96b116ad73ba29f43054e6e19a862d
Anton Krupin <krupin_anton@mail.ru>
2022-11-02 21:02:53 +0300

    add import { selectMessagesByChannel } from '../slices/selectors';

--- frontend/src/components/channelMessages.jsx
+++ frontend/src/components/channelMessages.jsx
@@ -1,16 +1,25 @@
 import { useSelector } from 'react-redux';
+import { selectMessagesByChannel } from '../slices/selectors';
 
 
 const ChannelMessages = () => {
-	const channelId = useSelector((state) => state.channels.activeChannel.id);
+	/*const channelId = useSelector((state) => state.channels.activeChannel.id);
 
 	const messages = useSelector((state) => state.messages.messages);
 
-	const channelMessages = messages.filter((message) => message.channelId === channelId);
+	const channelMessages = messages.filter((message) => message.channelId === channelId);*/
 
+	const channelMessages = useSelector(selectMessagesByChannel);
+	
 	return (
 		<>
-			{ channelMessages.map((message, index) => <div key={index} className="text-break mb-2"><b>{message.username}: {message.body}</b></div>) }
+			{ channelMessages.map((message) => 
+				<div
+					key={message.id}
+					className="text-break mb-2">
+						<b>{message.username}: {message.body}</b>
+				</div>)
+			}
 		</>
 	);
 }
