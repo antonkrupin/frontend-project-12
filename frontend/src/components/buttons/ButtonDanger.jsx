db3ca170589ec806822aecb65a42bba505f3d3bb
Anton Krupin <krupin_anton@mail.ru>
2022-11-04 17:40:11 +0300

    delete buttons component

--- frontend/src/components/buttons/ButtonDanger.jsx
+++
@@ -1,8 +0,0 @@
-const ButtonDanger = (props) => {
-	const { buttonText, buttonHandler } = props;
-	return (
-		<button className="btn btn-danger" onClick={buttonHandler}>{buttonText}</button>
-	)
-};
-
-export default ButtonDanger;
--- frontend/src/components/buttons/ButtonPrimary.jsx
+++
@@ -1,8 +0,0 @@
-const ButtonPrimary = (props) => {
-	const { buttonText, buttonHandler } = props;
-	return (
-		<button className="btn btn-primary" onClick={buttonHandler}>{buttonText}</button>
-	)
-};
-
-export default ButtonPrimary;
