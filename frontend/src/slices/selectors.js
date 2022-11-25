23ee1ac4e868e52a0e2bcb86f78bdf45834f4409
antonkrupin <krupin_anton@mail.ru>
2022-11-25 15:12:59 +0300

    add many selectors

--- frontend/src/slices/selectors.js
+++ frontend/src/slices/selectors.js
@@ -1,9 +1,27 @@
 import { createSelector } from '@reduxjs/toolkit';
 
 export const channelId = state => state.channels.activeChannel.id;
+
+export const channelForDeleteId = state => state.channels.channelForDelete.id;
+
+export const fetchChannelForRename = state => state.channels.channelForRename;
+
+export const loadingChannelsStatus = state => state.channels.loadingChannelsStatus;
+
+export const fetchChannelStatus = state => state.channels.channelStatus;
+
+export const fetchChannels = state => state.channels.channels;
+
+export const fetchChannelsNames = state => state.channels.channels.map(({name}) => name);
+
 export const messages = state => state.messages.messages;
 
+export const fetchMessagesStatus = state => state.messages.messagesStatus;
+
 export const modalType = state => state.modals.modalType;
+
+export const fetchError = state => state.errors.error;
+
 /*export const isAddChannelModalShow = state => state.modals.isAddChannelModalShow;
 export const isRenameChannelModalShow = state => state.modals.isRenameChannelModalShow;
 export const isDeleteChannelModalShow = state => state.modals.isDeleteChannelModalShow;*/
