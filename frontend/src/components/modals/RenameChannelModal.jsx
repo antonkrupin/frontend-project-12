cb938903053bef35487be22e120e92c062c5417d
Anton Krupin <krupin_anton@mail.ru>
2022-11-29 12:32:22 +0300

    add label

--- frontend/src/components/modals/RenameChannelModal.jsx
+++ frontend/src/components/modals/RenameChannelModal.jsx
@@ -85,6 +85,7 @@ const RenameChannelModal = (props) => {
 						ref={inputRef}
 						required
 					/>
+					<label className="channelName" htmlFor="channelName">Имя канала</label>
 					<ErrorsDiv errorText={useSelector(fetchError)}/>
 				</form>
 			</Modal.Body>
