3200c39a5c02d849a7f8648a30bf25e81c451c92
Anton Krupin <krupin_anton@mail.ru>
2022-12-01 16:40:13 +0300

    change

--- frontend/src/routes/chat.jsx
+++ frontend/src/routes/chat.jsx
@@ -4,113 +4,112 @@ import { ToastContainer, toast } from 'react-toastify';
 
 import { loadingChannelsStatus, fetchMessagesStatus } from '../slices/selectors';
 
-import  i18n from '../asserts/i18';
+import i18n from '../asserts/i18';
 import ChannelsList from '../components/channelsList';
 import ChannelName from '../components/channelName';
 import ChannelWindow from '../components/channelWindow';
 import ChannelMessages from '../components/channelMessages';
 
 import {
-	fetchMessages,
-	setUserName,
-	addMessage
+  fetchMessages,
+  setUserName,
+  addMessage,
 } from '../slices/messagesReducer';
 import {
-	setChannelStatus,
-	fetchChannels,
-	addChannel,
-	setActiveChannel,
-	renameChannel,
-	deleteChannel
+  setChannelStatus,
+  fetchChannels,
+  addChannel,
+  setActiveChannel,
+  renameChannel,
+  deleteChannel,
 } from '../slices/channelsReducer';
 
 const Chat = (props) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const { socket } = props;
+  const { socket } = props;
 
-	const channelsStatus = useSelector(loadingChannelsStatus);
+  const channelsStatus = useSelector(loadingChannelsStatus);
 
-	const messagesStatus = useSelector(fetchMessagesStatus);
-	
-	const notify = (text) => {
-		toast.success(text, {
-			position: "top-right",
-			autoClose: 5000,
-			theme: "light",
-		});
-	}
+  const messagesStatus = useSelector(fetchMessagesStatus);
 
-	useEffect(() => {
-		const username = JSON.parse(localStorage.getItem('userId')).username;
-		
-		dispatch(setUserName(username));
-		dispatch(fetchChannels());
-		dispatch(fetchMessages());
+  const notify = (text) => {
+    toast.success(text, {
+      position: 'top-right',
+      autoClose: 5000,
+      theme: 'light',
+    });
+  };
 
-		socket.on('newMessage', (payload) => {
-			dispatch(addMessage(payload));
-		});
+  useEffect(() => {
+    const { username } = JSON.parse(localStorage.getItem('userId'));
 
-		socket.on('newChannel', (payload) => {
-			dispatch(setActiveChannel(payload));
-			dispatch(addChannel(payload));
-			//dispatch(addChannelModalShow());
-			notify(i18n.t('ui.toasts.channelCreated'));
-			dispatch(setChannelStatus('added'));
-		});
+    dispatch(setUserName(username));
+    dispatch(fetchChannels());
+    dispatch(fetchMessages());
 
-		socket.on('renameChannel', (payload) => {
-			dispatch(renameChannel(payload));
-			//dispatch(renameChannelModalShow());
-			notify(i18n.t('ui.toasts.channelRenamed'));
-			dispatch(setChannelStatus('renamed'));
-		});
+    socket.on('newMessage', (payload) => {
+      dispatch(addMessage(payload));
+    });
 
-		socket.on('removeChannel', (payload) => {
-			dispatch(deleteChannel(payload));
-			//dispatch(deleteChannelModalShow());
-			notify(i18n.t('ui.toasts.channelDeleted'));
-			dispatch(setChannelStatus(null));
-		});
-	
-	}, []);
-	
+    socket.on('newChannel', (payload) => {
+      dispatch(setActiveChannel(payload));
+      dispatch(addChannel(payload));
+      // dispatch(addChannelModalShow());
+      notify(i18n.t('ui.toasts.channelCreated'));
+      dispatch(setChannelStatus('added'));
+    });
+
+    socket.on('renameChannel', (payload) => {
+      dispatch(renameChannel(payload));
+      // dispatch(renameChannelModalShow());
+      notify(i18n.t('ui.toasts.channelRenamed'));
+      dispatch(setChannelStatus('renamed'));
+    });
+
+    socket.on('removeChannel', (payload) => {
+      dispatch(deleteChannel(payload));
+      // dispatch(deleteChannelModalShow());
+      notify(i18n.t('ui.toasts.channelDeleted'));
+      dispatch(setChannelStatus(null));
+    });
+  }, []);
+
   return (
-		<>
-			{(channelsStatus === 'loading' || messagesStatus === 'loading') && 
-				<div className="d-flex flex-column justify-content-center align-items-center">
-					<div className="spinner-border text-primary" role="status">
-					</div>
-					<span className="sr-only text-primary">Загрузка...</span>
-				</div>
-			}
-			{(channelsStatus === 'resolved' && messagesStatus === 'resolved') && 
-			<>
-				<div className="container h-100 my-4 overflow-hidden rounded shadow">
-					<div className="row h-100 bg-white flex-md-row">
-						<div className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
-							<ChannelsList socket={socket}/>
-						</div>
-						<div className="col p-0 h-100">
-							<div className="d-flex flex-column h-100">
-								<div className="bg-light mb-4 p-3 shadow-sm small">
-									<ChannelName socket={socket}/>
-								</div>
-								<div id="messages-box" className="chat-messages overflow-auto px-5">
-									<ChannelMessages socket={socket}/>
-								</div>
-								<div className="mt-auto px-5 py-3">
-									<ChannelWindow socket={socket}/>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<ToastContainer />
-			</>}
-		</>
-	)
-}
+    <>
+      {(channelsStatus === 'loading' || messagesStatus === 'loading') && (
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <div className="spinner-border text-primary" role="status" />
+          <span className="sr-only text-primary">Загрузка...</span>
+        </div>
+      )}
+      {(channelsStatus === 'resolved' && messagesStatus === 'resolved') && (
+      <>
+        <div className="container h-100 my-4 overflow-hidden rounded shadow">
+          <div className="row h-100 bg-white flex-md-row">
+            <div className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
+              <ChannelsList socket={socket} />
+            </div>
+            <div className="col p-0 h-100">
+              <div className="d-flex flex-column h-100">
+                <div className="bg-light mb-4 p-3 shadow-sm small">
+                  <ChannelName socket={socket} />
+                </div>
+                <div id="messages-box" className="chat-messages overflow-auto px-5">
+                  <ChannelMessages socket={socket} />
+                </div>
+                <div className="mt-auto px-5 py-3">
+                  <ChannelWindow socket={socket} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ToastContainer />
+      </>
+      )}
+    </>
+  );
+};
 
 export default Chat;
