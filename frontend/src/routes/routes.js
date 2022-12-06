b56bccbb0360a775b2a2bf5a8765c50626bcebbb
antonkrupin <krupin_anton@mail.ru>
2022-12-06 10:36:45 +0300

    create routes.js

---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,13 @@
+const loginApiPath = '/api/v1/login';
+const signUpApiPath = '/api/v1/signup';
+const loginPagePath = 'login';
+const signUpPagePath = 'signup';
+const page404Path = '*';
+
+export {
+  loginApiPath,
+  signUpApiPath,
+  loginPagePath,
+  signUpPagePath,
+  page404Path,
+};
