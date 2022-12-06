6204197ac4c1d10a8a8649c0af4f8457a1411017
antonkrupin <krupin_anton@mail.ru>
2022-12-06 11:32:47 +0300

    create useSocket();

--- frontend/src/hooks/index.jsx
+++ frontend/src/hooks/index.jsx
@@ -1,7 +1,9 @@
 import { useContext } from 'react';
 
 import authContext from '../contexts/index.jsx';
+import SocketContext from '../contexts/socketContext.jsx';
 
 const useAuth = () => useContext(authContext);
+export const useSocket = () => useContext(SocketContext);
 
 export default useAuth;
