dac4af42140e997fe2c765b8dc99fa44394e069b
antonkrupin <krupin_anton@mail.ru>
2022-12-07 12:07:23 +0300

    add const messagesApiPath = '/api/v1/data';

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,5 +1,6 @@
 const loginApiPath = '/api/v1/login';
 const signUpApiPath = '/api/v1/signup';
+const messagesApiPath = '/api/v1/data';
 const loginPagePath = 'login';
 const signUpPagePath = 'signup';
 const page404Path = '*';
@@ -7,6 +8,7 @@ const page404Path = '*';
 export {
   loginApiPath,
   signUpApiPath,
+  messagesApiPath,
   loginPagePath,
   signUpPagePath,
   page404Path,
