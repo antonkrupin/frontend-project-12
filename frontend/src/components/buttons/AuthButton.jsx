61d046f0ab1e589b6258a530ea8e3fd075fbb72e
antonkrupin <krupin_anton@mail.ru>
2022-12-12 15:30:25 +0300

    add using location for render buttons

--- frontend/src/components/buttons/AuthButton.jsx
+++ frontend/src/components/buttons/AuthButton.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+import useAuth from '../../hooks';
 
 import i18 from '../../asserts/i18';
 
-const AuthButton = (props) => {
-  const { status } = props;
-  switch (status) {
-    case 'nonAuthorized': {
-      return (
+const AuthButton = () => {
+  const { pathname } = useLocation();
+
+  const auth = useAuth();
+
+  switch (pathname) {
+    case '/login': {
+      return auth.status === 'authorization' ? (
+        <button type="submit" className="w-100 mb-3 btn btn-outline-primary disabled">
+          <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+          {i18.t('ui.loginForm.buttonClicked')}
+        </button>
+      ) : (
         <button type="submit" className="w-100 mb-3 btn btn-outline-primary">
           {i18.t('ui.loginForm.button')}
         </button>
       );
     }
-    case 'authorization': {
-      return (
+    case '/signup': {
+      return auth.status === 'registration' ? (
         <button type="submit" className="w-100 mb-3 btn btn-outline-primary disabled">
           <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
-          {i18.t('ui.loginForm.buttonClicked')}
+          {i18.t('ui.signupForm.buttonClicked')}
         </button>
-      );
-    }
-    case 'nonRegistred': {
-      return (
+      ) : (
         <button type="submit" className="w-100 mb-3 btn btn-outline-primary">
           {i18.t('ui.signupForm.button')}
         </button>
       );
     }
-    case 'registration': {
-      return (
-        <button type="submit" className="w-100 mb-3 btn btn-outline-primary disabled">
-          <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
-          {i18.t('ui.signupForm.buttonClicked')}
-        </button>
-      );
-    }
     default: {
-      throw new Error('Unexpected status');
+      throw new Error('Unexpected location pathname');
     }
   }
 };
