e1f6d702dd5295c69e44f8132b00b3244cf11447
antonkrupin <krupin_anton@mail.ru>
2022-12-12 15:30:09 +0300

    change

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import useAuth from '../hooks';
 import i18n from '../asserts/i18';
 import LogOutButton from './buttons/LogOutButton';
 
 const NavBar = () => (
   <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
     <div className="container">
-      <Navbar.Brand as={Link} to="/">{i18n.t('ui.chatName')}</Navbar.Brand>
+      <Navbar.Brand as={Link} to={useAuth().status !== ('authorization' || 'registration') ? '/' : '#'}>{i18n.t('ui.chatName')}</Navbar.Brand>
       <LogOutButton />
     </div>
   </nav>
 );
 
 export default NavBar;
+
+/*
+
+<Link to={useAuth().status !== 'authorization' || 'registration' ? '/' : '#'}>
+                    {i18.t('ui.loginForm.registration')}
+                  </Link>
+(useAuth().status !== 'registration') && (useAuth().status !== 'authorization')
+*/
