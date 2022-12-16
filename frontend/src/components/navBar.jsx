7ac4fe2a982cddb6075d333db9af50957b6f677a
antonkrupin <krupin_anton@mail.ru>
2022-12-16 12:24:50 +0300

    change link dosen`t work, when registration or authorization

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -6,21 +7,16 @@ import useAuth from '../hooks';
 import i18n from '../asserts/i18';
 import LogOutButton from './buttons/LogOutButton';
 
-const NavBar = () => (
-  <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-    <div className="container">
-      <Navbar.Brand as={Link} to={useAuth().status !== ('authorization' || 'registration') ? '/' : '#'}>{i18n.t('ui.chatName')}</Navbar.Brand>
-      <LogOutButton />
-    </div>
-  </nav>
-);
+const NavBar = () => {
+  const { status } = useAuth();
+  return (
+    <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+      <div className="container">
+        <Navbar.Brand as={Link} to={(status !== 'authorization' && status !== 'registration') ? '/' : '#'}>{i18n.t('ui.chatName')}</Navbar.Brand>
+        <LogOutButton />
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
-
-/*
-
-<Link to={useAuth().status !== 'authorization' || 'registration' ? '/' : '#'}>
-                    {i18.t('ui.loginForm.registration')}
-                  </Link>
-(useAuth().status !== 'registration') && (useAuth().status !== 'authorization')
-*/
