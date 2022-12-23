909fb75af9f707db456c271e4494ccc10f42a892
antonkrupin <krupin_anton@mail.ru>
2022-12-23 10:44:27 +0300

    add routes mainPagePath: () => '/', emptyPath: () => '#',

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -4,6 +4,8 @@ export default {
   loginPath: () => [apiPath, 'login'].join('/'),
   signUpPath: () => [apiPath, 'signup'].join('/'),
   messagesPath: () => [apiPath, 'data'].join('/'),
+  mainPagePath: () => '/',
+  emptyPath: () => '#',
   loginPagePath: () => '/login',
   signUpPagePath: () => '/signup',
   page404Path: () => '*',
