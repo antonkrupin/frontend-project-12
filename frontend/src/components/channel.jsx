c7def0c98f0f6cf6cf958a5174f4d15c1c9ab8f3
Anton Krupin <krupin_anton@mail.ru>
2022-12-24 18:02:59 +0300

    remove comments

--- frontend/src/components/channel.jsx
+++ frontend/src/components/channel.jsx
@@ -46,46 +46,6 @@ const Channel = (props) => {
       </button>
     </li>
   );
-
-  /*
-  let channelItem = (
-    <li className="nav-item w-100">
-      <button
-        type="button"
-        onClick={onClick}
-        className={isActiveChannel
-          ? 'btn-secondary w-100 rounded-0 text-start btn'
-          : 'w-100 rounded-0 text-start btn'}
-      >
-        <span className="me-1 text-break">
-          #
-          {' '}
-          {channel.name}
-        </span>
-      </button>
-    </li>
-  );
-  if (channel.removable) {
-    channelItem = (
-      <li className="nav-item w-100">
-        <DropDownMenu
-          onClick={onClick}
-          channel={channel}
-          dropDownId={dropDownId}
-          dropDownClassName={isActiveChannel
-            ? 'btn-secondary flex-grow-0 dropdown-toggle dropdown-toggle-split border-0 btn'
-            : 'flex-grow-0 dropdown-toggle dropdown-toggle-split border-0 btn'}
-          channelClassName={isActiveChannel
-            ? 'btn-secondary w-100 rounded-0 text-start text-truncate border-0 btn'
-            : 'w-100 rounded-0 text-start text-truncate border-0 btn'}
-        />
-      </li>
-    );
-  }
-
-  return (
-    channelItem
-  ); */
 };
 
 export default Channel;
